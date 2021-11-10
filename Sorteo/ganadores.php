e0b29a3e28de86eafb01253ce49ed7314452d18a
RusbelCalderon <68758034+RusbelCalderon@users.noreply.github.com>
2021-11-10 12:33:14 -0500

    2 commit

--- Sorteo/ganadores.php
+++ Sorteo/ganadores.php
@@ -1,12 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/text.css" rel="stylesheet" />
+    <script type="text/javascript" src='./js/jquery.min.js'></script>
+    <script type="text/javascript" src='./js/bootstrap.min.js'></script>
+    <script type="text/javascript" src="./js/index.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
+
+
     <title>Document</title>
 </head>
 <body>
+<?php
     
+    include 'conexion.php';
+       
+        $consulta =     "SELECT a.nombres, a.apellidos , p.nombre_premio FROM ganadores g 
+        inner join asesores a on g.id_cedula = a.cedula
+        inner join premios p on g.id_premio = p.id";
+    
+        $resultado = mysqli_query( $conn, $consulta );
+        $consultaBD =  $resultado;
+        
+    ?>
+
+
+<table id="table_fixed">
+    <thead>
+      <tr>      
+        <th>NOMBRE</th>
+        <th>PREMIO</th>
+      </tr>
+    </thead>
+</table>
+<div id="contain">  
+  <table id="table_scroll">
+    <tbody>
+        <?php
+        while ($valores = mysqli_fetch_array($consultaBD)){
+                echo "<tr>";
+                echo "<td>".$valores['nombres']." ".$valores['apellidos']."</td>";
+                echo  "<td>".$valores['nombre_premio']."</td>" ;
+                echo "</tr>";
+            }
+        ?>
+        
+    </tbody>
+</table>
+</div>
 </body>
 </html>
--- Sorteo/js/index.js
+++ Sorteo/js/index.js
@@ -1,6 +1,7 @@
 var ganadores = [];
 var nivel;
 
+
 var closeAnimacionWiner = undefined;
 sndAdrs = undefined;
 
@@ -1102,3 +1103,41 @@ controller.init();
 requestAnimationFrame(controller.animation);
 }
 
+$(document).ready(function() {
+  
+	pageScroll();
+	$("#contain").mouseover(function() {
+	  clearTimeout(my_time);
+	}).mouseout(function() {
+	  pageScroll();
+	});
+	
+	getWidthHeader('table_fixed','table_scroll');
+	
+  });
+  
+  var my_time;
+  function pageScroll() {
+	  var objDiv = document.getElementById("contain");
+	objDiv.scrollTop = objDiv.scrollTop + 1;  
+	if ((objDiv.scrollTop + 100) == objDiv.scrollHeight) {
+	  objDiv.scrollTop = 0;
+	}
+	my_time = setTimeout('pageScroll()', 25);
+  }
+  
+  function getWidthHeader(id_header, id_scroll) {
+	var colCount = 0;
+	$('#' + id_scroll + ' tr:nth-child(1) td').each(function () {
+	  if ($(this).attr('colspan')) {
+		colCount += +$(this).attr('colspan');
+	  } else {
+		colCount++;
+	  }
+	});
+	
+	for(var i = 1; i <= colCount; i++) {
+	  var th_width = $('#' + id_scroll + ' > tbody > tr:first-child > td:nth-child(' + i + ')').width();
+	  $('#' + id_header + ' > thead th:nth-child(' + i + ')').css('width',th_width + 'px');
+	}
+  }
